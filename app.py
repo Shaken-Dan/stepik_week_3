6a3d25a7a1eca1b76134909a3cfe8498763a8c3d
Shd-Dan <shd.cs.ch@gmail.com>
2024-01-31 18:38:31 +0600

    did it

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import json
-
-from flask import Flask, render_template, abort
+import os
+from flask import Flask, render_template, abort, flash, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, HiddenField
+from wtforms import StringField, IntegerField, SubmitField, HiddenField, RadioField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.secret_key = "afasfasfaf848a4sf8as41f5a1sf15"
+
+days = {'mon': 'Понедельник',
+        'tue': "Persembe",
+        'wed': "Среда",
+        'thu': "Четверг",
+        'fri': "Жұма",
+        'sat': "Сенбі",
+        'sun': "Воскресенье"
+        }
 
 
 class UserForm(FlaskForm):
@@ -15,6 +25,21 @@ class UserForm(FlaskForm):
     clientWeekday = HiddenField()
     clientTime = HiddenField()
     clientTeacher = HiddenField()
+
+
+class RequestForm(FlaskForm):
+    objective = RadioField(
+        'travel',
+        choices=[('goal1', 'Для путешествий'), ('goal2', 'Для школы'), ('goal3', 'Для работы'),
+                 ('goal4', 'Для переезда')],
+        validators=[DataRequired()])
+    time_budget = RadioField(
+        'time',
+        choices=[('time1', '1-2 часа в неделю'), ('time2', '3-5 часов в неделю'), ('time3', '5-7 часов в неделю'),
+                 ('time4', '7-10 часов в неделю')],
+        validators=[DataRequired()])
+    name = StringField("Вас зовут", validators=[DataRequired()])
+    phone = IntegerField("Ваш телефон", validators=[DataRequired()])
 
 
 @app.route('/')
@@ -31,7 +56,11 @@ def all_view():
 # Преподаватели по цели изучения
 @app.route('/goals/<goal>')
 def goal_view(goal):
-    return render_template('goal.html')
+    with open("teacher_db.json", "r", encoding="utf-8") as file:
+        profiles_db = json.load(file)
+        goal = goal
+
+    return render_template('goal.html', profiles_db=profiles_db, goal=goal)
 
 
 # Страница репетитора
@@ -65,26 +94,56 @@ def profile_view(id):
 # заявки на подбор
 @app.route('/request')
 def request_view():
-    return render_template('request.html')
+    form = RequestForm()
+    return render_template('request.html', form=form)
 
 
 # заявка на подбор отправлена
-@app.route('/request_done')
+@app.route('/request_done', methods=['GET', 'POST'])
 def request_done_view():
-    return render_template('request_done.html')
+    form = RequestForm()
+    if request.method == 'POST':
+        client_data = {
+            "name": form.name.data,
+            "phone": form.phone.data,
+            "objective": dict(form.objective.choices).get(form.objective.data),
+            "time": dict(form.time_budget.choices).get(form.time_budget.data)
+        }
+        try:
+            # Check if the JSON file exists and is not empty
+            if os.path.exists("request.json") and os.path.getsize("request.json") > 0:
+                # Load existing data from the JSON file
+                with open("request.json", "r", encoding="utf-8") as file:
+                    data = json.load(file)
+            else:
+                # Initialize data as an empty dictionary if the file is empty or doesn't exist
+                data = {}
+        except json.decoder.JSONDecodeError:
+            # Handle JSONDecodeError if the file is empty or not properly formatted
+            data = {}
+
+        # Find the next available index
+        index = max(map(int, data.keys()), default=0) + 1
+
+        # Append new client data to the dictionary
+        data[str(index)] = client_data
+
+        with open("request.json", "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+            file.write('\n')
+
+        return render_template('request_done.html',
+                               objective=client_data['objective'],
+                               time=client_data['time'],
+                               name=client_data['name'],
+                               phone=client_data['phone'])
+    else:
+        return render_template('request.html', form=form)
 
 
 # здесь будет форма бронирования <id учителя>
 @app.route('/booking/<int:tutor_id>/<string:day>/<time>', methods=['GET', 'POST'])
 def booking_view(tutor_id, day, time):
-    days = {'mon': 'Понедельник',
-            'tue': "Persembe",
-            'wed': "Среда",
-            'thu': "Четверг",
-            'fri': "Жұма",
-            'sat': "Сенбі",
-            'sun': "Воскресенье"
-            }
     form = UserForm()
     form.clientWeekday.data = day
     form.clientTime.data = time
@@ -108,9 +167,34 @@ def booking_view(tutor_id, day, time):
 
 
 # заявка отправлена
-@app.route('/booking_done')
+@app.route('/booking_done', methods=['POST', 'GET'])
 def booking_done_view():
-    return render_template('booking_done.html')
+    form = UserForm()
+    post_in_day = form.clientWeekday.data
+    booked_day = ''
+    for key, value in days.items():
+        if post_in_day == key:
+            booked_day = value
+    booked_time = form.clientTime.data + ':00'
+    client_name = form.clientName.data
+    client_phone = form.clientPhone.data
+
+    booking_data = {
+        "booked_time": booked_time,
+        "client_name": client_name,
+        "client_phone": client_phone
+    }
+
+    with open('booking.json', 'a', encoding='utf-8') as file:
+        json.dump(booking_data, file, indent=4, ensure_ascii=False)
+        file.write('\n')
+    flash("Data is recorded")
+    return render_template('booking_done.html',
+                           booked_day=booked_day,
+                           booked_time=booked_time,
+                           client_name=client_name,
+                           client_phone=client_phone
+                           )
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import json
 
+goal = "study"
 with open("teacher_db.json", "r", encoding="utf-8") as file:
     data_base = json.load(file)
-    teacher_name = data_base[0]['name']
-
-
+    # teacher_name = data_base[0]['name']
+    for profile in data_base:
+        if goal in profile['goals']:
+            print(profile['id'])
